d2c1059302c8d767cd8b62bff8bf6268bcd31ce9
OskarPrzybylski <przybylski.oskar@gmail.com>
2019-03-07 20:55:48 +0100

    Added implementation of tails function

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,24 @@
 package edu.iis.mto.lab_0;
 
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
-    public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+    public List<String> tails(String value)
+    {
+        if(value == null) {
+            return Collections.emptyList();
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i< value.length();i++){
+            list.add(value.substring(i));
+        }
+
+        list.add("");
+        return list;
     }
 
 }
